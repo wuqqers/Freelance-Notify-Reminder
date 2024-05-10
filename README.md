1323a5b634bfd3043f4ccb493a07dc110be6d59e
wuqqers <62030865+wuqqers@users.noreply.github.com>
2024-05-10 13:05:25 +0300

    Go

--- README.md
+++ README.md
@@ -1,2 +1,72 @@
-# Freelance-reminder
- For Bionluk Only
+# Bionluk Mesaj Bildirimi Uygulaması
+
+Bu uygulama, Bionluk platformunda yeni mesajlar geldiğinde kullanıcıyı bilgilendirmek için geliştirilmiş bir bildirim uygulamasıdır.
+
+## Başlangıç
+
+Uygulamayı kullanmaya başlamadan önce, aşağıdaki adımları izleyerek gereksinimleri kurmanız gerekmektedir.
+
+### Gereksinimler
+
+- Python 3.x
+- Firefox web tarayıcısı
+- `pip` paket yöneticisi
+
+### Kurulum
+
+## 1.Yöntem:  Projeyi bilgisayarınıza klonlayın: 
+
+  
+    git clone https://github.com/kullanici_adi/bionluk-mesaj-bildirimi.git
+  
+
+2. Proje dizinine gidin:
+
+    ```bash
+    cd bionluk-mesaj-bildirimi
+    ```
+
+3. Gerekli Python paketlerini yükleyin:
+
+    ```bash
+    pip install -r requirements.txt
+    ```
+
+4. Proje dizinindeki `.env` dosyasını düzenleyin bionluk giriş bilgilerinizi bilgilerinizi ekleyin ve kaydedin.
+
+5. Uygulamayı çalıştırın:
+
+    ```bash
+    python main.py
+    ```
+
+
+## 2.Yöntem
+1. Paketlenmiş exe dosyasını indirin:
+
+  ```bash  
+ https://github.com/wuqqers/BionlukAlarm/releases/tag/main 
+  ```
+2. Dosyayı zipden çıkarın 
+3. Proje dizinindeki `.env` dosyasını düzenleyin bionluk giriş bilgilerinizi bilgilerinizi ekleyin ve kaydedin.
+4. (Opsiyonel) MP3 dosyasını değiştirin 
+5. alarm.exe'yi çalıştırın
+
+
+
+## Kullanım
+
+Uygulama çalıştırıldığında, Bionluk hesabınıza otomatik olarak giriş yapacak ve yeni mesajları kontrol edecektir. Yeni bir mesaj geldiğinde, kullanıcıyı sesli bir bildirimle bilgilendirir.
+
+Ayrıca, uygulama sistem tepsisinde (system tray) çalışır ve pencereyi kapatmak, simgeyi gizlemek ve yeniden göstermek için sağ tık menüsüne erişim sağlar.
+
+## Katkılar
+
+Katkıda bulunmak isterseniz, lütfen bir çekme isteği (pull request) açın veya bir sorun (issue) bildirin.
+
+## Lisans
+
+Bu proje MIT lisansı altında lisanslanmıştır. Daha fazla bilgi için [LICENSE](LICENSE) dosyasına bakın.
+
+----
+
---
+++ Reminder.py
@@ -0,0 +1,150 @@
+import os
+import time
+import tkinter as tk
+from infi.systray import SysTrayIcon
+import pygame
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.firefox.options import Options
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from dotenv import load_dotenv
+
+load_dotenv()  # .env dosyasını yükle
+
+class BionlukApp:
+    icon_created = False
+
+    def __init__(self, root):
+        self.root = root
+        root.title("Bionluk Mesaj Bildirimi")
+        root.geometry("500x100")
+        root.iconbitmap('icon.ico')
+        self.message_label = tk.Label(root, text="Başlatılıyor...", font=("Arial", 14))
+        self.message_label.place(relx=0.5, rely=0.5, anchor="center")
+        self.options = Options()
+        self.music_start_time = None
+        self.notification_sound = None
+        self.options.headless = True
+        self.driver = webdriver.Firefox(options=self.options)
+        self.driver.set_window_size(1, 1)
+        root.bind("<Unmap>", self.minimize_to_tray)
+        root.protocol("WM_DELETE_WINDOW", self.minimize_to_tray) 
+        self.create_system_tray_icon()
+
+    def create_system_tray_icon(self):
+        if not BionlukApp.icon_created:
+            menu_options = (("Show/Maximize", None, self.show_maximize_window),)
+            self.icon = SysTrayIcon("icon.ico", "Bionluk Mesaj Bildirimi", menu_options, on_quit=self.on_quit)
+            self.icon.start()
+            BionlukApp.icon_created = True
+
+    def on_quit(self, systray=None):
+     pygame.mixer.quit()
+     self.driver.quit()
+     self.root.quit()  # Tkinter penceresini kapat
+     self.icon.visible = False  # SysTrayIcon'ı gizle
+     os._exit(0)
+
+
+
+
+
+    def show_maximize_window(self, systray=None):
+        self.icon.visible = False  # Simgeyi gizle
+        self.root.deiconify()  # Pencereyi görünür yap
+        self.root.geometry("500x100")  # Pencere boyutunu ayarla
+
+
+
+
+    def minimize_to_tray(self, event=None):
+     self.icon.visible = False
+     self.root.withdraw()  # Pencereyi gizle
+
+
+
+
+    def play_notification_sound(self):
+        if self.notification_sound and self.notification_sound.get_num_channels() > 0:
+            return
+
+        pygame.mixer.init()
+        self.notification_sound = pygame.mixer.Sound("bildirim.mp3")
+        self.notification_sound.play()
+        self.music_start_time = time.time()
+        self.check_music_status()
+
+    def check_music_status(self):
+        if self.notification_sound is None or self.notification_sound.get_num_channels() == 0:
+            return
+        else:
+            music_length = self.notification_sound.get_length()
+            remaining_time = self.music_start_time + music_length - time.time()
+            if remaining_time > 0:
+                self.root.after(int(remaining_time * 1000), self.check_music_status)
+            else:
+                if self.driver.find_elements(By.CSS_SELECTOR, "span.button-badge.unread_message_count") and \
+                        self.driver.find_elements(By.CSS_SELECTOR, "span.button-badge.unread_message_count")[0].text.strip():
+                    self.play_notification_sound()
+
+    def start_app(self):
+        self.driver.get("https://bionluk.com/login")
+
+        message = "Giriş Yapılıyor..."
+        self.message_label.config(text=message)
+        username_input = WebDriverWait(self.driver, 10).until(
+            EC.presence_of_element_located((By.CSS_SELECTOR, "input[placeholder='E-posta veya kullanıcı adı']"))
+        )
+        password_input = self.driver.find_element(By.CSS_SELECTOR, "input[placeholder='Şifre']")
+        login_button = self.driver.find_element(By.CSS_SELECTOR, "button.super-button-green")
+
+        username = os.getenv("BIONLUK_USERNAME")
+        password = os.getenv("BIONLUK_PASSWORD")
+
+        username_input.send_keys(username)
+        password_input.send_keys(password)
+        login_button.click()
+
+        message = "Giriş Yapıldı, Mesajlar Kontrol Ediliyor"
+        self.message_label.config(text=message)
+
+        self.check_messages()
+
+    def check_messages(self):
+        unread_message_count = self.driver.find_elements(By.CSS_SELECTOR, "span.button-badge.unread_message_count")
+
+        if unread_message_count:
+            message = "Yeni mesajlar var!"
+            self.play_notification_sound()
+        else:
+            message = "Yeni mesaj yok."
+
+        self.message_label.config(text=message)
+
+        if unread_message_count and unread_message_count[0].text.strip():
+            self.root.after(60000, self.check_messages)
+        else:
+            self.root.after(60000, self.check_messages)
+        
+        # Sayfanın yenilenmesi gerekip gerekmediğini kontrol et
+        if self.check_if_page_needs_refresh():
+            self.refresh_page()
+
+    def check_if_page_needs_refresh(self):
+        # Belirli bir div'in varlığını kontrol et
+        warning_div = self.driver.find_elements(By.CSS_SELECTOR, "div.version-warning")
+        return len(warning_div) > 0
+
+    def refresh_page(self):
+        # Sayfayı yenile
+        self.driver.refresh()
+
+    def run(self):
+        self.root.after(2000, self.start_app)
+        self.root.mainloop()
+
+if __name__ == "__main__":
+    root = tk.Tk()
+    app = BionlukApp(root)
+    app.run()
