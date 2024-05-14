d550ee5a110224a1921f859eb93ab46e9fefb1f3
wuqqers <62030865+wuqqers@users.noreply.github.com>
2024-05-14 15:40:34 +0300

    Update Reminder.py

--- Reminder.py
+++ Reminder.py
@@ -9,6 +9,7 @@ from selenium.webdriver.firefox.options import Options
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from dotenv import load_dotenv
+import threading
 
 load_dotenv()  # .env dosyasını yükle
 
@@ -20,17 +21,18 @@ class BionlukApp:
         root.title("Bionluk Mesaj Bildirimi")
         root.geometry("500x100")
         root.iconbitmap('icon.ico')
-        self.message_label = tk.Label(root, text="Başlatılıyor...", font=("Arial", 14))
+        self.message_label = tk.Label(root, text="", font=("Arial", 14))
         self.message_label.place(relx=0.5, rely=0.5, anchor="center")
         self.options = Options()
         self.music_start_time = None
         self.notification_sound = None
         self.options.headless = True
-        self.driver = webdriver.Firefox(options=self.options)
-        self.driver.set_window_size(1, 1)
+        self.driver = None  # Tarayıcıyı burada başlatmayacağız
         root.bind("<Unmap>", self.minimize_to_tray)
-        root.protocol("WM_DELETE_WINDOW", self.minimize_to_tray) 
+        root.protocol("WM_DELETE_WINDOW", self.minimize_to_tray)
         self.create_system_tray_icon()
+        self.start_app_thread = threading.Thread(target=self.start_app)
+        self.start_app_thread.start()
 
     def create_system_tray_icon(self):
         if not BionlukApp.icon_created:
@@ -40,30 +42,21 @@ class BionlukApp:
             BionlukApp.icon_created = True
 
     def on_quit(self, systray=None):
-     pygame.mixer.quit()
-     self.driver.quit()
-     self.root.quit()  # Tkinter penceresini kapat
-     self.icon.visible = False  # SysTrayIcon'ı gizle
-     os._exit(0)
-
-
-
-
+        pygame.mixer.quit()
+        if self.driver:  # Tarayıcı mevcutsa kapat
+            self.driver.quit()
+        self.root.quit()  # Tkinter penceresini kapat
+        self.icon.visible = False  # SysTrayIcon'ı gizle
+        os._exit(0)
 
     def show_maximize_window(self, systray=None):
         self.icon.visible = False  # Simgeyi gizle
         self.root.deiconify()  # Pencereyi görünür yap
         self.root.geometry("500x100")  # Pencere boyutunu ayarla
 
-
-
-
     def minimize_to_tray(self, event=None):
-     self.icon.visible = False
-     self.root.withdraw()  # Pencereyi gizle
-
-
-
+        self.icon.visible = False
+        self.root.withdraw()  # Pencereyi gizle
 
     def play_notification_sound(self):
         if self.notification_sound and self.notification_sound.get_num_channels() > 0:
@@ -89,10 +82,12 @@ class BionlukApp:
                     self.play_notification_sound()
 
     def start_app(self):
+        self.update_message("Giriş Yapılıyor...")
+        self.driver = webdriver.Firefox(options=self.options)
+        self.driver.set_window_size(1, 1)  # Tarayıcıyı başlat
+
         self.driver.get("https://bionluk.com/login")
 
-        message = "Giriş Yapılıyor..."
-        self.message_label.config(text=message)
         username_input = WebDriverWait(self.driver, 10).until(
             EC.presence_of_element_located((By.CSS_SELECTOR, "input[placeholder='E-posta veya kullanıcı adı']"))
         )
@@ -106,30 +101,41 @@ class BionlukApp:
         password_input.send_keys(password)
         login_button.click()
 
-        message = "Giriş Yapıldı, Mesajlar Kontrol Ediliyor"
-        self.message_label.config(text=message)
+        self.update_message("Giriş Yapıldı, Mesajlar Kontrol Ediliyor")
 
-        self.check_messages()
+        self.check_messages_thread = threading.Thread(target=self.check_messages)
+        self.check_messages_thread.start()
 
     def check_messages(self):
-        unread_message_count = self.driver.find_elements(By.CSS_SELECTOR, "span.button-badge.unread_message_count")
+        try:
+            while True:
+                # Mesajları kontrol et
+                unread_message_count = self.driver.find_elements(By.CSS_SELECTOR, "span.button-badge.unread_message_count")
 
-        if unread_message_count:
-            message = "Yeni mesajlar var!"
-            self.play_notification_sound()
-        else:
-            message = "Yeni mesaj yok."
+                if unread_message_count:
+                    message = "Yeni mesajlar var!"
+                    self.play_notification_sound()
+                else:
+                    message = "Yeni mesaj yok."
 
-        self.message_label.config(text=message)
+                self.update_message(message)
 
-        if unread_message_count and unread_message_count[0].text.strip():
-            self.root.after(60000, self.check_messages)
-        else:
-            self.root.after(60000, self.check_messages)
-        
-        # Sayfanın yenilenmesi gerekip gerekmediğini kontrol et
-        if self.check_if_page_needs_refresh():
-            self.refresh_page()
+                if unread_message_count and unread_message_count[0].text.strip():
+                    time.sleep(2)
+                else:
+                    time.sleep(2)
+
+                # Sayfanın yenilenmesi gerekip gerekmediğini kontrol et
+                if self.check_if_page_needs_refresh():
+                    self.refresh_page()
+        except Exception as e:
+            # Hata oluştuysa, tarayıcıyı yeniden başlat
+            print("Hata oluştu:", e)
+            self.driver.quit()
+            self.driver = None
+            self.root.update()
+            self.start_app_thread = threading.Thread(target=self.start_app)
+            self.start_app_thread.start()
 
     def check_if_page_needs_refresh(self):
         # Belirli bir div'in varlığını kontrol et
@@ -140,8 +146,11 @@ class BionlukApp:
         # Sayfayı yenile
         self.driver.refresh()
 
+    def update_message(self, message):
+        self.message_label.config(text=message)
+        self.root.update_idletasks()
+
     def run(self):
-        self.root.after(2000, self.start_app)
         self.root.mainloop()
 
 if __name__ == "__main__":
